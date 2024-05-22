a4e7c4905f61e1e4c63adb83ce1b66f744c86bf2
ghdwnstjd <dltmdgheorkf1@gmail.com>
2024-05-22 20:38:29 +0900

    2024.05.22 홍준성 로그인 페이지 첫 페이지로 변경 폰트 추가

--- esgproject/src/App.js
+++ esgproject/src/App.js
@@ -1,30 +1,33 @@
 import {Container, DataContainer} from "./App.styles.tsx";
-import { Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
 // 공통 영역
 import SideBar from './ESG-common/SideBar/p-esg-common-SideBar.tsx';
 import Navbar from './ESG-common/NavBar/p-esg-common-NavBar.tsx';
 
 // 화면 영역
+import Login from './ESG-login/p-esg-login.tsx';
 import Main from './ESG-form/main/p-esg-form-main.tsx';
 import Environmental from './ESG-form/test/p-esg-form-test.tsx';
 
 function App() {
 
-  return (      <div className="App" style={{backgroundColor:"#faf9f8"}}>
-  <Navbar />
-  <Container>
-    <SideBar />
-    <DataContainer>
-      <Routes>
-        {/* 실제 데이터 작성 구간 */}
-          <Route path="/" exact element={<Main/>}></Route>
-          <Route path="/environmental" element={<Environmental/>}></Route>
-        {/* 실제 데이터 작성 구간 */}
-      </Routes>
-    </DataContainer>
-  </Container>
-</div>
+  return (      
+  <div className="App" style={{backgroundColor:"#faf9f8"}}>
+    <Navbar />
+    <Container>
+      <SideBar />
+      <DataContainer>
+        <Routes>
+          <Route path="/" exact element={<Login/>}></Route>
+          {/* 실제 데이터 작성 구간 */}
+            <Route path="/main" element={<Main/>}></Route>
+            <Route path="/environmental" element={<Environmental/>}></Route>
+          {/* 실제 데이터 작성 구간 */}
+        </Routes>
+      </DataContainer>
+    </Container>
+  </div>
   );  
 }
 
--- esgproject/src/ESG-login/p-esg-login.tsx
+++ esgproject/src/ESG-login/p-esg-login.tsx
@@ -1,11 +1,20 @@
-import React, {useState}  from 'react';
+import React  from 'react';
 import styles from './p-esg-login.module.css';
+import { useNavigate  } from 'react-router-dom';
 
 import OurLogo from '../assets/image/belly.png';
 import EYLogo from '../assets/image/EYLogo.svg';
 import LoginPicture from '../assets/image/LoginPicture.jpg';
 
-const loginPage = () => {
+const LoginPage = () => {
+
+    const navigate = useNavigate();
+
+    // 로그인 관련 sp를 탈 때는 여기서 sp 결과 불러와서 진행
+    const goToMain = () => {
+        navigate("/main");
+    }
+
     return (
         <>
             <div className = {styles.LoginContainer}>
@@ -13,12 +22,12 @@ const loginPage = () => {
                     <div className = {styles.LoginLeft}>
                         <div className = {styles.LoginLeftTop}>
                             <div className = {styles.OurLogo}>
-                                <img className = {styles.OurLogo} src={OurLogo}/>
+                                <img className = {styles.OurLogo} src={OurLogo} alt={"OurLogo"}/>
                             </div>
                             <div className = {styles.OurName}>뽈록앤뽈록</div>
                         </div>
                         <div>
-                            <img className = {styles.LoginPicture} src={LoginPicture} />
+                            <img className = {styles.LoginPicture} src={LoginPicture} alt={"LoginPicture"}/>
                         </div>
                         <div className = {styles.LoginLeftBottom}>ESG Data Platform</div>
                         
@@ -26,12 +35,12 @@ const loginPage = () => {
                     <div className = {styles.LoginRight}>
                         <div className = {styles.LoginRightWrap}>
                             <div className={styles.EYLogoWrap}>
-                                <img className = {styles.EYLogo} src={EYLogo}></img>
+                                <img className = {styles.EYLogo} src={EYLogo} alt={"EYLogo"}></img>
                             </div>
                             <div className={styles.LoginInputWrap}>
                                 <input type="text" className = {styles.LoginInput} placeholder="사용자ID"></input>
                                 <input type="text" className = {styles.LoginInput} placeholder="패스워드"></input>
-                                <button className = {styles.LoginBtn}>로그인</button>
+                                <button className = {styles.LoginBtn} onClick={goToMain}>로그인</button>
                             </div>
                             <div className={styles.copyright}>
                                 Copyright © 2024.Bblock. All rights reserved
@@ -45,4 +54,4 @@ const loginPage = () => {
     )
 }
 
-export default loginPage
+export default LoginPage
