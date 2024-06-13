5af45386b414b9b0d61fc15bfc767af1e4a27743
신은규 <dmsrb1021@naver.com>
2024-06-13 15:35:07 +0900

    2024.06.13 신은규 1. 다이얼로그 드래그 이동 제거

--- esgproject/src/ESG-common/Dialogue/p-esg-common-dialogue.tsx
+++ esgproject/src/ESG-common/Dialogue/p-esg-common-dialogue.tsx
@@ -1,16 +1,8 @@
 import React from 'react'
 import '../../global.d.ts';
 import styles from './p-esg-common-dialogue.module.css'
-import {useSpring, animated} from 'react-spring';
-import {useDrag} from 'react-use-gesture';
 
 const Dialogue = ({children, DlgName, isOpenDlg, Dlgwidth, Dlgheight , setIsOpen}) => {
-    const logoPos = useSpring({x:0, y:0});
-
-    const bindLogoPos = useDrag((params)=>{
-        logoPos.x.set(params.offset[0]);
-        logoPos.y.set(params.offset[1]);
-      });
 
     const closeDlg = () => {
         setIsOpen(false);
@@ -20,19 +12,15 @@ const Dialogue = ({children, DlgName, isOpenDlg, Dlgwidth, Dlgheight , setIsOpen
     return (
         isOpenDlg &&
         <div className={styles.DialogueWarp}>
-            <animated.div className={styles.Dialogue} style={{width : Dlgwidth? Dlgwidth : "600px", height: Dlgheight ? Dlgheight : "500px",
-                x: logoPos.x,
-                y: logoPos.y,
-                cursor: 'Default'
-            }}>
-                <div className ={styles.DlgHeader} {...bindLogoPos()}>
+            <div className={styles.Dialogue} style={{width : Dlgwidth? Dlgwidth : "600px", height: Dlgheight ? Dlgheight : "500px"}}>
+                <div className ={styles.DlgHeader}>
                     <div className={styles.DlgName}>{DlgName}</div>
                     <div className={styles.CloseBtn} onClick={closeDlg}/>
                 </div>
                 <div className ={styles.DlgContent} style={{width : Dlgwidth? Dlgwidth : "600px", height: Dlgheight ? Dlgheight - 25 : "475px"}}>
                     {children}
                 </div>
-            </animated.div>
+            </div>
         </div>
     )
 } 
