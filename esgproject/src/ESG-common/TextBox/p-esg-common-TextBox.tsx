a58c3de534ac9aa1df53777c0e4469b2320c12da
LAPTOP-Q27FB0N0\USER <junsungh1601@gmail.com>
2026-01-25 10:06:44 +0900

    [2026.01.25] 조회조건 텍스트박스랑 데이트피커 길이가 안맞아서 모바일용 수정

--- esgproject/src/ESG-common/TextBox/p-esg-common-TextBox.tsx
+++ esgproject/src/ESG-common/TextBox/p-esg-common-TextBox.tsx
@@ -6,7 +6,20 @@ import styles from './p-esg-common-TextBox.module.css';
 const TextBox = (settings : any) => {
     const [text, setText] = useState(settings.value || '');
     const [isReadOnly] = useState(settings.readOnly || false);
+    const [isMobile, setIsMobile] = React.useState(window.innerWidth <= 768);
 
+    useEffect(() => {
+    const handleResize = () => {
+        setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+        window.removeEventListener('resize', handleResize);
+    };
+    }, []);
+    
     useEffect(() => {
         setText(settings.value || '');
     }, [settings.value]);
@@ -31,7 +44,18 @@ const TextBox = (settings : any) => {
             <div className={styles.TextBoxWrap}>
                 <div className={styles.TextBoxTitle} style={{color: settings.isRequire? "red" : "rgb(144, 144, 144)"}}>{settings.name? settings.name : 'Default'}</div>
                 <div className={styles.InputWrap}>
-                    <input className={styles.Input} type="text" style={{width: settings.width? settings.width : 200}} onChange={changeText} value={text} readOnly={settings.readOnly === true}></input>  
+                    <input
+                    className={styles.Input}
+                    type="text"
+                    style={{
+                        width: isMobile
+                        ? 200                      // 모바일은 무조건 200
+                        : settings.width ?? 200    // PC는 입력 값, 없으면 200
+                    }}
+                    onChange={changeText}
+                    value={text}
+                    readOnly={settings.readOnly === true}
+                    />
                     {!isReadOnly &&
                         <button className={styles.BtnClear} onClick={RemoveText} style={{display: text.length > 0 ? "inline-block" : "none"}}></button>
                     }
